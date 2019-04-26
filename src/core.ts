133776e718f3841f2b678606c97b9b3ae260d4d2
Thomas Tumiel <ttumiel@gmail.com>
2019-04-26 11:02:13 +0200

    add Env docstrings

--- src/core.ts
+++ src/core.ts
@@ -23,15 +23,58 @@ import * as tf from "@tensorflow/tfjs";
  *      reward_range: A tuple corresponding to the min and max possible rewards
  */
 interface Env {
+  /**
+   * @property action_space The possible actions that can be taken. 
+   *           either continuous or discrete.
+   */
   action_space: Space;
+
+  /**
+   * @property observation_space The observable world.
+   */
   observation_space: Space;
+
+  /**
+   * @property reward_range The possible rewards an agent can achieve.
+   */
   reward_range: Space;
 
+  /**
+   * @function step Steps the environment according to some action.
+   * @param action The action to take (in action_space)
+   * @returns [tf.Tensor, number, boolean, {}]
+   */
   step(action: number): [tf.Tensor, number, boolean, {}];
+
+  /**
+   * @function step Overloaded step for Phaser Games.
+   * @param time Phaser time
+   * @param delta Phaser time delta
+   * @param action The action to take (in action_space)
+   * @returns [tf.Tensor, number, boolean, {}]
+   */
   step(time: number, delta: number, action: number): [tf.Tensor, number, boolean, {}];
+
+  /**
+   * @function reset Restore the environment to a random starting state
+   * @returns tf.Tensor The initial observation
+   */
   reset(): tf.Tensor;
+
+  /**
+   * @function render Display the game environment
+   */
   render(): void;
+
+  /**
+   * @function close Terminate the game session and close environment
+   */
   close(): void;
+
+  /**
+   * @function seed Seed the randomness in the environment
+   * @param seed The seed value
+   */
   seed(seed: number): void;
 }
 
