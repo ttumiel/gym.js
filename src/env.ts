65ee0926755bb306f7ce665e2741716ba1fa02c2
Thomas Tumiel <ttumiel@gmail.com>
2019-06-21 21:36:26 +0200

    add exports to root

---
+++ src/env.ts
@@ -0,0 +1,20 @@
+// Toy text envs
+import FrozenLake from "./envs/text/FrozenLake";
+
+export {FrozenLake}
+// -----------------------------------------------
+
+
+// Algorithmic envs
+import Copy from "./envs/algorithmic/Copy";
+import Reverse from "./envs/algorithmic/Reverse";
+
+export {Copy, Reverse}
+// -----------------------------------------------
+
+
+// Arcade envs
+import Snake from "./envs/arcade/snake/SnakeGame";
+
+export {Snake}
+// -----------------------------------------------
