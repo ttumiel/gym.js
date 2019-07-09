e42975676c38e9202367f93d72f303e0d6e41c67
Thomas Tumiel <ttumiel@gmail.com>
2019-07-09 12:03:46 +0200

    test wrapper

---
+++ src/test/core.test.ts
@@ -0,0 +1,32 @@
+import { Wrapper } from '../env';
+import {FrozenLake} from "../env";
+
+
+describe('Wrapper', () => {
+  let env = new FrozenLake(4,0.8,false);
+  let copyEnv = new FrozenLake(4,0.8,false);
+  let wrap = new Wrapper(env);
+
+  it('reset', ()=>{
+    expect(wrap.reset().dataSync())
+      .toEqual(copyEnv.reset().dataSync())
+  })
+
+  it('step', ()=>{
+    let action = 1;
+    let [obs,rew,done,info] = wrap.step(action);
+    let [envObs,envRew,envDone,envInfo] = copyEnv.step(action);
+
+    expect(obs.dataSync())
+      .toEqual(envObs.dataSync())
+
+    expect(rew)
+      .toEqual(envRew)
+
+    expect(done)
+      .toEqual(envDone)
+
+    expect(info)
+      .toEqual(envInfo)
+  })
+});
