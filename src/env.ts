10c8e7b25261f54f6169baa4f7be0810fb128099
Thomas Tumiel <ttumiel@gmail.com>
2019-07-13 23:34:36 +0200

    rearrange exports

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,31 @@
+// Environments
+// ===============================================
+
 // Toy text envs
 import FrozenLake from './envs/text/FrozenLake';
-
 export { FrozenLake };
 // -----------------------------------------------
 
 // Algorithmic envs
 import Copy from './envs/algorithmic/Copy';
 import Reverse from './envs/algorithmic/Reverse';
-
 export { Copy, Reverse };
 // -----------------------------------------------
 
 // Arcade envs
-import Snake from './envs/arcade/snake/SnakeGame';
-
-export { Snake };
+// import Snake from './envs/arcade/snake/SnakeGame';
+// export { Snake };
 // -----------------------------------------------
 
-// Core
-import Env, { Wrapper } from './core';
+// ===============================================
 
-export { Env, Wrapper };
+// Core
+import Env, { Wrapper, ObservationWrapper } from './core';
+export { Env, Wrapper, ObservationWrapper };
+// -----------------------------------------------
+
+// Spaces
+import Space from "./spaces/space";
+import Discrete, {DiscreteTuple} from './spaces/discrete';
+export {Space, Discrete, DiscreteTuple};
+// -----------------------------------------------
